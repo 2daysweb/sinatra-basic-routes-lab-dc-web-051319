a7be038abe7d9ef820f275ff7e3acd87195c7250
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-01 21:34:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get('/name'){"My name is ___"}
-  get('/hometown'){"My name is ___"}
-  get('/favorite-song'){"My name is ___"}
+  get('/hometown'){"My hometown is ___"}
+  get('/favorite-song'){"My favorite song is ___"}
   
   
 end
